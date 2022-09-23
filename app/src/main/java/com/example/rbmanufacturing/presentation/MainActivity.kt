4b7a8823972878b1f812fd5953b75855b50581ac
Oleg Kvashnin <olkv@wellit.pro>
2022-09-23 15:24:36 +0600

    Move RecyleView in fragment

--- app/src/main/java/com/example/rbmanufacturing/presentation/MainActivity.kt
+++ app/src/main/java/com/example/rbmanufacturing/presentation/MainActivity.kt
@@ -16,8 +16,6 @@ import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var vmMain: MainViewModel
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -28,27 +26,11 @@ class MainActivity : AppCompatActivity() {
 
     private fun init() {
 
-        vmMain = ViewModelProvider(this)[MainViewModel::class.java]
-
         val txtOperation = findViewById<TextView>(R.id.txtOperation)
 
-        val rcvFunOperation = findViewById<RecyclerView>(R.id.rcvFunOperation)
-        val adapter = FunOperationAdapter(this)
-
-
-        rcvFunOperation.hasFixedSize()
-        rcvFunOperation.layoutManager = LinearLayoutManager(this)
-        rcvFunOperation.adapter = adapter
-
-        lifecycleScope.launch {
-            vmMain.listFunOperation.collect {
-
-                val listFunOperation = vmMain.GetListOperation()
-                adapter.setOperationArray(listFunOperation)
-
-            }
-        }
-
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.viewMain, OperationFragment.newInstance())
+            .commit()
 
     }
 
--- app/src/main/java/com/example/rbmanufacturing/presentation/OperationFragment.kt
+++ app/src/main/java/com/example/rbmanufacturing/presentation/OperationFragment.kt
@@ -1,17 +1,21 @@
 package com.example.rbmanufacturing.presentation
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.rbmanufacturing.R
+import kotlinx.coroutines.launch
 
 class OperationFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+    private lateinit var vmMain: MainViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -19,6 +23,29 @@ class OperationFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_operation, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        vmMain = ViewModelProvider(this)[MainViewModel::class.java]
+
+        val rcvFunOperation = view?.findViewById<RecyclerView>(R.id.rcvFunOperation)
+        val adapter = FunOperationAdapter(view.context)
+
+        rcvFunOperation?.hasFixedSize()
+        rcvFunOperation?.layoutManager = LinearLayoutManager(view.context)
+        rcvFunOperation?.adapter = adapter
+
+        lifecycleScope.launch {
+            vmMain.listFunOperation.collect {list ->
+
+                //Log.d("MYLOG", "Количество записей: ${list.size.toString()}")
+                adapter.setOperationArray(list)
+
+            }
+        }
+
     }
 
     companion object {
