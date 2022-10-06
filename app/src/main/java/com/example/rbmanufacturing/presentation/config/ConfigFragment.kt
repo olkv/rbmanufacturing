b494ffb2a1310d66405578eb9cac1b5a852bc45f
Oleg Kvashnin <olkv@wellit.pro>
2022-10-06 18:40:49 +0600

    Add save config in SharedPreferense

--- app/src/main/java/com/example/rbmanufacturing/presentation/config/ConfigFragment.kt
+++ app/src/main/java/com/example/rbmanufacturing/presentation/config/ConfigFragment.kt
@@ -1,12 +1,17 @@
 package com.example.rbmanufacturing.presentation.config
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
+import androidx.constraintlayout.widget.SharedValues
 import com.example.rbmanufacturing.R
 import com.google.android.material.textfield.TextInputEditText
 
@@ -19,10 +24,6 @@ class ConfigFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        userName = "oleg"
-        userPassword = "12345"
-        urlConnect = "http://192.168.1.1/"
 
     }
 
@@ -38,6 +39,12 @@ class ConfigFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        val sharedPreferences = view.context.getSharedPreferences("RbManConfig",Context.MODE_PRIVATE)
+
+        urlConnect = sharedPreferences.getString("urlConnect","http://192.168.1.1/")!!
+        userPassword = sharedPreferences.getString("userPassword","")!!
+        userName = sharedPreferences.getString("userName","noname")!!
+
         val txtURLConnect = view.findViewById<TextInputEditText>(R.id.editURLConnect)
         txtURLConnect.setText(urlConnect)
 
@@ -46,6 +53,17 @@ class ConfigFragment : Fragment() {
 
         val txtUserPassword = view.findViewById<EditText>(R.id.editUserPassword)
         txtUserPassword.setText(userPassword)
+
+        val btnSaveConfig = view.findViewById<Button>(R.id.btnSaveConfig)
+        btnSaveConfig.setOnClickListener {
+            val editConfig = sharedPreferences.edit()
+            editConfig.putString("urlConnect", txtURLConnect.text.toString())
+            editConfig.putString("userPassword",txtUserPassword.text.toString())
+            editConfig.putString("userName",txtUserName.text.toString())
+            Toast.makeText(it.context,"Настройки сохранены",Toast.LENGTH_SHORT).show()
+            editConfig.commit()
+        }
+
 
     }
 
